daabffbae6a627260b940fefe98c393f0ce6cbb6
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-06-10 17:31:50 +0200

    movimiento horizontal y vertical

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// ------------------------ 1. creamos canvas ------------------------
 //Creating canvas
 const canvas = document.querySelector("#gameCanvas");
 //2 dimentional drawing context
 const ctx = canvas.getContext("2d");
 
-//Creating the snake
+
+// ------------------------ 2. creamos la serpiente ------------------------
+//Hacemos variable con las coordenadas que ocupará la serpiente dentro del canvas
 let snake = [ {x:300, y:300}, {x:280, y:300}, {x:260, y:300}, {x:240, y:300}, {x:220, y:300} ];
 
-//Un círculo:
+//Cómo se dibuja un círculo?
 /*
 ctx.beginPath();
 ctx.fillStyle = "#ffabf6";
@@ -15,7 +18,7 @@ ctx.fill();
 ctx.closePath();
 */
 
-// Un cuadrado:
+//Cómo se dibuja un cuadrado?
 /*
 ctx.fillStyle = 'lightblue';  
 ctx.strokestyle = 'darkblue';
@@ -23,16 +26,16 @@ ctx.fillRect(300, 300, 10, 10);
 ctx.strokeRect(300, 300, 10, 10);
 */
 
-//Cadena de círculos para snakeBody   <----- no funciona!!!!!!!
+//Función que dibuja un círculo en cada objeto de la array snake
 function drawSnakeBody(snakePart) {
 	ctx.beginPath();
 	ctx.fillStyle = "#ffabf6";
 	ctx.arc(snakePart.x, snakePart.y, 10, 0, 2*Math.PI);
 	ctx.fill();
 }
-
+//Función que vincula cada círculo de la función anterior con cada objeto del array. Esto nos dibuja el cuerpo de la serpiente
 function drawSnake() {
-	console.log("entra");
+	//console.log("entra");
 	snake.forEach((snakePart)=> drawSnakeBody(snakePart));
 	/*for (snakePart of snake){
 		drawSnakeBody();
@@ -40,13 +43,12 @@ function drawSnake() {
 }
 drawSnake();
 
-/*
-//Cadena con cuadrados
-function drawSnakeBody(snakePart) 
-{  
-  ctx.fillStyle = 'lightblue';  
-  ctx.strokestyle = 'darkblue';
-  ctx.fillRect(snakePart.x, snakePart.y, 10, 10);  
-  ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+
+// ------------------------ 3. El movimiento la serpiente ------------------------
+//horizontal movement
+function moveSnake() {
+	const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+	snake.unshift(head);
+	snake.pop();
 }
-*/
+
